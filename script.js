3bf5041805eddbb6fefa5b539bec3313f156074c
vale0153 <vale0153@algonquinlive.com>
2022-10-20 17:17:38 -0400

    Added CSS

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const $emojis =
 const emojis = []
 
 for (let i=129410; i<=129460; i++){
-  emojis.push(`<div class="emoji">${i}</div>`)
+  emojis.push(`<div class="emoji">&#${i}</div>`)
 }
 
 $emojis.innerHTML += emojis.join('')
